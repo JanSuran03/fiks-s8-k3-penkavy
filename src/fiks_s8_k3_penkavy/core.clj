42c90e9458aae2960f2cac8983b47e742ac43380
JanSuran03 <62071148+JanSuran03@users.noreply.github.com>
2021-12-19 23:27:13 +0100

    Number builder and byte builder added.

--- src/fiks_s8_k3_penkavy/core.clj
+++ src/fiks_s8_k3_penkavy/core.clj
@@ -1,6 +1,60 @@
-(ns fiks-s8-k3-penkavy.core)
+(ns fiks-s8-k3-penkavy.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ascii-num-diff (- (int \0) 0))
+
+(def ascii->number (->> (range 10) (map #(vector (char (+ % ascii-num-diff))
+                                                 %))
+                        (into {})))
+(def number-chars (set (map first ascii->number)))
+
+(defn is-return?
+  "In \"ISO-8859-1\" encoding, there ALWAYS is \return and \newline right after."
+  [ch]
+  (= \return ch))
+
+(defn process-pure-input [pure-input])
+
+(defn number-builder [char-sequence]
+  (loop [[^Character firstch & input] char-sequence
+         number ""]
+    (cond (contains? number-chars firstch)
+          (recur input
+                 (str number firstch))
+
+          (and (str/blank? number)
+               (Character/isWhitespace firstch))
+          (recur input
+                 number)
+
+          :else
+          [(read-string number)
+           input])))
+
+(defn byte-builder
+  "Returns: [byte-length byte input-rest]"
+  [input]
+  (let [[byte-length input] (number-builder input)
+        need-chars (Math/ceil (/ byte-length 4))]
+    (loop [need-chars need-chars
+           [firstch & remaining-input] input
+           byte-ret []]
+      (if (zero? need-chars)
+        [byte-length byte-ret (rest remaining-input)]
+        (recur (dec need-chars)
+               remaining-input
+               (conj byte-ret firstch))))))
+
+(defn read-and-process-input []
+  (let [pure-input (slurp "sample.txt" :encoding "ISO-8859-1")
+        [num-inputs pure-input] (number-builder pure-input)
+        process-input (fn [input]
+                        (let [[incoming-lines rest-input] (number-builder input)
+                              [dna-diff-tolerance rest-input] (number-builder rest-input)]
+                          (loop [remaining-inputs incoming-lines
+                                 ret []]
+                            (byte-builder (rest rest-input)))))]
+    (process-input pure-input)))
+
+(defn -main [& _args]
+  (read-and-process-input))
