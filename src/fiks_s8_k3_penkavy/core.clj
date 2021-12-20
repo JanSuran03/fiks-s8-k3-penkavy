5e6b18e389f7c64f6ddc5e156928813f47d23f16
JanSuran03 <62071148+JanSuran03@users.noreply.github.com>
2021-12-20 23:00:48 +0100

    Sample input working.

--- src/fiks_s8_k3_penkavy/core.clj
+++ src/fiks_s8_k3_penkavy/core.clj
@@ -1,14 +1,36 @@
 (ns fiks-s8-k3-penkavy.core
   (:import (java.io ByteArrayOutputStream)))
 
-(def byte->number (->> (range 10) (map (fn [b]
-                                         [(+ b 48) b]))
-                       (into {})))
+(defn dec->bin [n]
+  (loop [ret ()
+         n n]
+    (if (pos? n)
+      (recur (conj ret (rem n 2))
+             (quot n 2))
+      ret)))
+
+(def nucleus-basis {'(0 0) "A"
+                    '(0 1) "C"
+                    '(1 0) "G"
+                    '(1 1) "T"})
+
+(defn fill-zeros [byte-seq]
+  (loop [byte-seq byte-seq
+         filled (rem (count byte-seq) 4)]
+    (if (= 4 filled)
+      byte-seq
+      (recur (cons 0 byte-seq)
+             (inc filled)))))
+
+(defn dec->8-bit-binary-pairs [n]
+  (->> n dec->bin
+       fill-zeros
+       (partition 2)))
 
 (def zero 48)
 (def nine 57)
-(defn number-byte? [b]
-  (<= zero b nine))
+(def byte->number (->> (range 10) (map (fn [b] [(+ b 48) b])) (into {})))
+(defn number-byte? [b] (<= zero b nine))
 
 ;; https://clojuredocs.org/clojure.core/slurp
 (defn slurp-bytes
@@ -18,9 +40,6 @@
     (clojure.java.io/copy (clojure.java.io/input-stream x) out)
     (->> (.toByteArray out)
          (map #(Byte/toUnsignedInt ^Byte %)))))
-
-(defn is-newline? [ch]
-  (= \newline ch))
 
 (defn number-builder [byte-seq]
   (loop [[firstb & more] byte-seq
@@ -57,22 +76,36 @@
                                                [dna input-rest] (finch-dna-builder dna-length input-rest)]
                                            (recur (dec num-remaining-finches)
                                                   input-rest
-                                                  (conj! ret {:dna-length         dna-length
-                                                              :dna                dna
-                                                              :dna-diff-tolerance dna-diff-tolerance})))
-                                         [(persistent! ret) remaining-input]))))]
+                                                  (conj! ret {:dna-length dna-length
+                                                              :dna        dna})))
+                                         [{:dna-diff-tolerance dna-diff-tolerance
+                                           :finches            (persistent! ret)} remaining-input]))))]
     (loop [remaining-inputs num-inputs
            ret (transient [])
            remaining-input pure-input]
       (if (zero? remaining-inputs)
         (persistent! ret)
-        (let [[input-ret remaining-input] (process-finches-sequence remaining-input)]
+        (let [[input-ret remaining-input]
+              (process-finches-sequence remaining-input)]
           (recur (dec remaining-inputs)
                  (conj! ret input-ret)
                  remaining-input))))))
+
+(defn analyze-finches-seq [{:keys [dna-diff-tolerance
+                                   finches] :as _finches-seq}]
+  (let [analyzed-finches-seq (for [{:keys [dna-length dna]} finches]
+                               (let [nucleus-basis (->> dna (mapcat #(->> % dec->8-bit-binary-pairs
+                                                                          (map nucleus-basis)))
+                                                        (take dna-length)
+                                                        (apply str))]
+                                 nucleus-basis))]
+    {:dna-diff-tolerance dna-diff-tolerance
+     :finches            analyzed-finches-seq}))
+
 (defn read-and-process-input [filename]
-  (let [input-as-bytes (slurp-bytes filename)]
-    (process-input input-as-bytes)))
+  (let [input-as-bytes (slurp-bytes filename)
+        processed-input (process-input input-as-bytes)]
+    (map analyze-finches-seq processed-input)))
 
 (defn -main [filename]
   (read-and-process-input filename))
--- src/fiks_s8_k3_penkavy/maybe_simpler.clj
+++ src/fiks_s8_k3_penkavy/maybe_simpler.clj
@@ -1,17 +1,1 @@
 (ns fiks-s8-k3-penkavy.maybe-simpler)
-
-(defn dec->bin [n]
-  (loop [ret ()
-         n n]
-    (if (pos? n)
-      (recur (conj ret (rem n 2))
-             (quot n 2))
-      ret)))
-
-(defn fill-zeros [byte-seq]
-  (loop [byte-seq byte-seq
-         filled (rem (count byte-seq) 4)]
-    (if (= 4 filled)
-      byte-seq
-      (recur (cons 0 byte-seq)
-             (inc filled)))))
